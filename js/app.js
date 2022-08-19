5b39569570d1ee4312ed585352ac8fc4613721cb
Andres Sepulveda <andresfelipesepulveda@live.com>
2022-08-19 08:22:35 -0500

    Comments added to the code

--- js/app.js
+++ js/app.js
@@ -7,13 +7,13 @@
 
 //Event Listeners
 document.addEventListener('DOMContentLoaded', () => {
-  obtenerTopCryptos()
+  obtenerTopCryptos()//Al cargar el dom llama la funcion de consulta de las top 10 criptomonedas
   formulario.addEventListener('submit', validarFormulario)
 })
 
 
 //Funciones
-function obtenerTopCryptos() {
+function obtenerTopCryptos() { //Consulta en la api cuales son las 10 criptomonedas con mayor marketcap
   const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`
   fetch(url)
     .then(respuesta => respuesta.json())
@@ -21,9 +21,9 @@ function obtenerTopCryptos() {
 
 }
 
-function llenarCryptos(datos) {
+function llenarCryptos(datos) { //Recibe el objeto de las top 10 criptomonedas y llena el select de la interfaz con los datos
   datos.forEach(element => {
-    const {FullName, Name} = element.CoinInfo;
+    const {FullName, Name} = element.CoinInfo; //Destructuring del objeto CoinInfo para extraer el FullName y Name
     const select = document.createElement('option')
     select.value = Name;
     select.textContent = FullName;
@@ -31,7 +31,7 @@ function llenarCryptos(datos) {
   })
 }
 
-function validarFormulario(e) {
+function validarFormulario(e) { //Valida que ambos campos tengan una seleccion
   e.preventDefault()
   if(selectCrypto.value == '' || selectMoneda.value == '') {
     mostrarAlerta('Primero elige una criptomoneda y una moneda', 'error')
@@ -40,7 +40,7 @@ function validarFormulario(e) {
   consultarCambio(selectCrypto.value, selectMoneda.value)
 }
 
-function mostrarAlerta(mensaje, tipo) {
+function mostrarAlerta(mensaje, tipo) { //Muestra un mensaje al ususario
   const alertExiste = document.querySelector('.error')
   if(alertExiste) {
     return;
@@ -56,7 +56,7 @@ function mostrarAlerta(mensaje, tipo) {
   }, 3000);
 }
 
-function consultarCambio(crypto, moneda) {
+function consultarCambio(crypto, moneda) {//Consulta en la API la información de la criptomoneda seleccionada
   mostrarSpinner()
   const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${crypto}&tsyms=${moneda}`
   fetch(url)
@@ -64,9 +64,8 @@ function consultarCambio(crypto, moneda) {
     .then(datos => mostrarResultado(datos.DISPLAY[crypto][moneda], crypto, moneda))
 }
 
-function mostrarResultado(conversion, crypto) {
+function mostrarResultado(conversion, crypto) { //Muestra en interfaz los datos obtenidos de la API
   limpiarHTML()
-  console.log(conversion);
   resultado.innerHTML = `
   <h3 class="text-white text-base md:text-lg lg:text-xl mb-3">El precio actual de ${crypto} es: <span class="font-bold text-lg lg:text-2xl">${conversion.PRICE}</span></h3>
   <p class="text-white text-sm md:text-base lg:text-lg mb-2">El precio más alto de hoy fue: <span class="font-bold">${conversion.HIGHDAY}</span></p>
@@ -82,7 +81,7 @@ function limpiarHTML() {
   }
 }
 
-function mostrarSpinner() {
+function mostrarSpinner() { //Muestra un spinner de carga 
   limpiarHTML();
   const spinner = document.createElement('div');
   spinner.classList.add('sk-fading-circle');
